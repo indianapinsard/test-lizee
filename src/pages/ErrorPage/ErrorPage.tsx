f0fc3b2a43e3828b86cb7c3d6988d3486af87f9f
Indi <indiana.pinsard@wattys-conseil.com>
2022-11-18 22:20:56 +0100

    build error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,11 +1,22 @@
-import './ErrorPage.css';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+import styles from './ErrorPage.module.css';
 
 function ErrorPage(): JSX.Element {
   return (
-    <div className="page error-page">
-      error page
+    <div className={styles.page}>
+      <Icon fontSize="large">report_problem</Icon>
+      <h1 className={styles.message}>Oops, something went wrong ...</h1>
+      <Button
+        size="large"
+        variant="outlined"
+        component={Link}
+        to="/"
+        sx={{borderRadius: '24px'}}>Revenir à l'accueil</Button>
     </div>
   );
 }
 
 export default ErrorPage;
+
